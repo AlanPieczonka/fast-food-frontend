0468088da11988c95e7c5016f73f73b6027ab271
Drakeoon <tmlm18@gmail.com>
2019-05-19 00:48:41 +0200

    Fix serving issues

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import Parse from './config/parse';
 
 class App extends Component {
-  static state = {
+  state = {
     products: []
   }
 
