b005cded3f0c33e312f6727cebe7d04e9024e4a4
Drakeoon <tmlm18@gmail.com>
2019-05-19 12:56:19 +0200

    Add Sidebar component

---
+++ src/components/layout/Sidebar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Sidebar extends Component {
+  render() {
+    return (
+      <div class="sidebar">
+        Sidebar Content
+      </div>
+    )
+  }
+}
