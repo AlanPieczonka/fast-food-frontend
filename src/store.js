20cab7e7fd200a7365c0c716810a8e622f0ab470
Alan Pieczonka <alanpieczonka@gmail.com>
2019-05-25 20:38:00 +0200

    Configure redux-devtools-extension

--- src/store.js
+++ src/store.js
@@ -1,13 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga'
 import rootSaga from './sagas'
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from './rootReducer';
 
 const sagaMiddleware = createSagaMiddleware()
 const store = createStore(
   rootReducer,
-  applyMiddleware(sagaMiddleware)
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
 )
 sagaMiddleware.run(rootSaga)
 
