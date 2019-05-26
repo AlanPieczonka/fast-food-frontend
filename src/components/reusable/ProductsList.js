3cb7de4dfe8199be0ea80d0dd08cc7b08645f734
Drakeoon <tmlm18@gmail.com>
2019-05-26 21:23:26 +0200

    Add graphql query to the products list

--- src/components/reusable/ProductsList.js
+++ src/components/reusable/ProductsList.js
@@ -1,26 +1,39 @@
-import React, { Component } from 'react'
-import Parse from '../../config/parse'
+import React from 'react'
+import gql from 'graphql-tag'
+import { Query } from 'react-apollo'
 
-export default class ProductsList extends Component {
-  state = {
-    products: []
+const GET_PRODUCTS = gql`
+  query {
+    products {
+      name
+    }
   }
+`;
 
-  componentDidMount() {
-    this.fetchProducts()
-  }
+export default () => (
+  <Query query={GET_PRODUCTS}>
+    {({ loading, error, data }) => {
+      if (loading) return 'Loading...';
+      if (error) return `Error! ${error}`;
 
-  fetchProducts = async () => {
-    const query = new Parse.Query('Products');
-    const products = await query.find()
-    this.setState({products})
-  }
+      return data.products.map(({ name, price }) => (
+        <div className="p-card --border-bottom">
+          <img className="p-card__thumbnail" src="https://placehold.it/100x100" alt="Product Thumbnail" />
+          
+          <div className="p-card__content">
+            <h2 className="p-card__heading">{name}</h2>
 
-  render() {
-    const { products } = this.state
-    const PRODUCTS_LIST = products.length
-      ? products.map(product => <div key={product.id}>{product.get('displayName')}</div>)
-      : "Loading..."
-    return PRODUCTS_LIST
-  }
-}
+            <div className="p-card__footer">
+              <span className="p-card__quantity">
+                <input type="submit" value="-"/>
+                <input type="text" value="1"/>
+                <input type="submit" value="+"/>
+              </span>
+              <span className="p-card__price">{price} zł</span>
+            </div>
+          </div>
+        </div>
+      ))
+    }}
+  </Query>
+)
