af617d04c9671a437711a5fe159dd437264fa6d6
Daniel Szczepanik <tmlm18@gmail.com>
2019-09-15 19:21:53 +0200

    Code refactor and logic cleanup

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -15,14 +15,40 @@ const initialProducts = [
 
 export default function Sidebebar () {
   const [products, setProducts] = useState(initialProducts)
+
   const sum = products.length ? products.map(({ price, quantity }) => price * quantity).reduce((x, y) => x + y).toFixed(2) : 0
+
+  const lowerProductQuantity = index => () => {
+    const updatedProducts = [...products]
+
+    if (updatedProducts[index].quantity <= 0)
+      return
+
+    updatedProducts[index].quantity -= 1
+
+    setProducts(updatedProducts)
+  }
+
+  const increaseProductQuantity = index => () => {
+    const updatedProducts = [...products]
+    updatedProducts[index].quantity += 1
+
+    setProducts(updatedProducts)
+  }
+
+  const removeProduct = index => () => {
+    const updatedProducts = [...products]
+    updatedProducts.splice(index, 1)
+
+    setProducts(updatedProducts)
+  }
 
   return (
     <div className="sidebar">
       <h2 className="sidebar__heading">Current Order</h2>
-      {
-        products.map(({ name, quantity, price }) => {
-        return (<div className="sidebar__basket">
+
+      {products.map(({ name, quantity, price }, index) => (
+        <div className="sidebar__basket" key={name}>
           <div className="p-card --border-bottom">
             <img className="p-card__thumbnail" src="https://placehold.it/100x100" alt="Product Thumbnail" />
             <div className="p-card__content">
@@ -32,37 +58,23 @@ export default function Sidebebar () {
                   <input 
                     type="submit" 
                     value="-" 
-                    onClick={() => {
-                      const updatedProducts = [...products]
-                      const index = updatedProducts.findIndex(product => product.name === name)
-                      if (updatedProducts[index].quantity !== 0) updatedProducts[index].quantity = updatedProducts[index].quantity - 1
-
-                      setProducts(updatedProducts)
-                    }} 
+                    onClick={lowerProductQuantity(index)} 
                   />
                   <input type="text" value={quantity} disabled/>
                   <input 
                     type="submit" 
                     value="+" 
-                    onClick={() => {
-                      const updatedProducts = [...products]
-                      const index = updatedProducts.findIndex(product => name === product.name)
-                      updatedProducts[index].quantity = updatedProducts[index].quantity + 1
-
-                      setProducts(updatedProducts)
-                    }}
+                    onClick={increaseProductQuantity(index)}
                   />
                 </span>
-                <input type="submit" value="X" onClick={() => {
-                  setProducts(products.filter((product) => product.name !== name))
-                }}/>
+                <input type="submit" value="X" onClick={removeProduct(index)}/>
                 <span className="p-card__price">{price} zł</span>
               </div>
             </div>
           </div>
         </div>
-        )})
-      }
+      ))}
+
       <div className="sidebar__footer">
         <div className="sidebar__summary">
           <span>Sum:</span>
