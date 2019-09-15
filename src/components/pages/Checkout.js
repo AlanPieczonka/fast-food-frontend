09ab196363521ab4ffa7f65e2513b73d0d16550c
Daniel Szczepanik <tmlm18@gmail.com>
2019-09-15 19:38:09 +0200

    Rearrange components for new routing

--- src/components/pages/Checkout.js
+++ src/components/pages/Checkout.js
@@ -1,11 +1,19 @@
 import React from 'react'
 
+import Sidebar from '../layout/Sidebar'
+import Content from '../layout/Content'
 import ProductsList from '../reusable/ProductsList'
 
 export default () => (
-  <div className='checkout'>
-    <div className='checkout__products'>
-      <ProductsList />
-    </div>
-  </div>
+  <>
+    <Sidebar />
+    
+    <Content>
+      <div className='checkout'>
+        <div className='checkout__products'>
+          <ProductsList />
+        </div>
+      </div>
+    </Content>
+  </>
 )
---
+++ src/components/pages/Management.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import ProductsList from '../reusable/ProductsList'
+
+export default () => (
+  <div className='checkout'>
+    Products management
+  </div>
+)
