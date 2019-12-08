b42948a0846e1cc2a5683624dbb4bbb0db282130
Daniel Szczepanik <tmlm18@gmail.com>
2019-12-08 18:48:18 +0100

    Use new atomic buttons in Product List

--- src/components/pages/Management/ProductList.js
+++ src/components/pages/Management/ProductList.js
@@ -1,20 +1,22 @@
-import React from 'react'
+import React from "react";
 
-import { Link } from 'react-router-dom'
-import List from '../../reusable/ProductList'
+import { Link } from "react-router-dom";
+import List from "../../reusable/ProductList";
 
 const ProductList = () => (
   <>
     <div className="management__toolbar">
-      <Link to="/management/new" className="management__toolbar-button">
-        Add a new product
-      </Link>
+      <div className="btn__group -end">
+        <Link to="/management/new" className="btn -outline -sm">
+          New product
+        </Link>
+      </div>
     </div>
 
     <div className="management__products">
       <List type="management" />
     </div>
   </>
-)
+);
 
-export default ProductList
+export default ProductList;
