1bcf92dc56dc091515fcfc820d7a10d1c417d609
Alan Pieczonka <alanpieczonka@gmail.com>
2020-04-01 23:11:16 +0200

    Move PaymentForm submit code to seperate function

--- src/components/reusable/PaymentForm.js
+++ src/components/reusable/PaymentForm.js
@@ -5,20 +5,25 @@ import { saveOrder } from "../../actionCreators/order"
 import { calculateSum } from "./../../utils/order"
 
 export default function PaymentForm({ order, closeModal }) {
-        const sum = calculateSum(order)
-        const dispatch = useDispatch()
+    const sum = calculateSum(order)
+    const dispatch = useDispatch()
 
-        return (
-            <div>
-                <ul>
-                    {order.map(({ name, quantity}) =>
-                        <li key={name}>
-                            Name: {name}, quantity: {quantity}
-                        </li>
-                    )}
-                </ul>
+    const handleSubmit = async () => {
+        await dispatch(saveOrder())
+        closeModal()
+    }
+
+    return (
+        <div>
+            <ul>
+                {order.map(({ name, quantity }) =>
+                    <li key={name}>
+                        Name: {name}, quantity: {quantity}
+                    </li>
+                )}
+            </ul>
                 Sum: {sum} zł
-                <button className='btn -block' onClick={() => dispatch(saveOrder()).then(() => closeModal())}>Pay</button>
-            </div>
-        )
+            <button className='btn -block' onClick={handleSubmit}>Pay</button>
+        </div>
+    )
 }
