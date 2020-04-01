09b6413ea7b25ea12ddc9c00f5b7c150588cf6f3
Alan Pieczonka <alanpieczonka@gmail.com>
2020-04-01 23:01:21 +0200

    Create /management/deals page

---
+++ src/components/pages/Management/Deals/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Deals = () => {
+    return (
+        <div>
+            <h1 className="text-center">Coupons &amp; Deals</h1>
+        </div>
+    )
+}
+
+export default Deals
