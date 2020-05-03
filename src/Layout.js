9d5ab72caa2323537eec0c2e33cccfb32dcfc7ca
Alan Pieczonka <alanpieczonka@gmail.com>
2020-05-03 19:36:07 +0200

    Rewrite Layout component to functional type, display notification and update state locally on product update

--- src/Layout.js
+++ src/Layout.js
@@ -1,5 +1,6 @@
-import React, { Component } from "react";
-
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { toast } from 'react-toastify'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { ToastContainer } from 'react-toastify';
 
@@ -7,41 +8,51 @@ import Navbar from "./components/layout/Navbar";
 import Checkout from "./components/pages/Checkout";
 import Management from "./components/pages/Management";
 import Orders from "./components/pages/Orders";
+import ProductUpdated from "./components/notifications/ProductUpdated";
 import withTheme from "./hocs/withTheme";
 
 import 'react-toastify/dist/ReactToastify.min.css';
 
-class Layout extends Component {
-  updateVh = () => {
+import useChannel from "./hooks/websockets";
+import { updateProductLocally } from "./actionCreators/product"
+
+const Layout = () => {
+  const updateVh = () => {
     let vh = window.innerHeight * 0.01;
 
     document.documentElement.style.setProperty("--vh", `${vh}px`);
   };
 
-  componentDidMount() {
-    this.updateVh();
+  const dispatch = useDispatch();
+  useChannel('product:update', (event, payload) => {
+    if (event === "product_updated") {
+      dispatch(updateProductLocally(parseInt(payload.id), payload.attributes))
+      toast(() => <ProductUpdated id={payload.id} attributes={payload.attributes} />)
+    }
+  })
 
-    this.resizeListener = window.addEventListener("resize", this.updateVh);
-  }
+  useEffect(() => {
+    updateVh();
 
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.resizeListener);
-  }
+    const resizeListener = window.addEventListener("resize", updateVh);
 
-  render() {
-    return (
-      <Router>
-        <Navbar />
+    return () => {
+      window.removeEventListener("resize", resizeListener);
+    }
+  }, [])
 
-        <Switch>
-          <Route path="/orders" component={Orders} />
-          <Route path="/management" component={Management} />
-          <Route path="/" component={Checkout} />
-        </Switch>
-        <ToastContainer />
-      </Router>
-    );
-  }
+  return (
+    <Router>
+      <Navbar />
+
+      <Switch>
+        <Route path="/orders" component={Orders} />
+        <Route path="/management" component={Management} />
+        <Route path="/" component={Checkout} />
+      </Switch>
+      <ToastContainer />
+    </Router>
+  );
 }
 
 export default withTheme(Layout);
