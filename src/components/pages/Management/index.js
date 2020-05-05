7dd19ca8f0b6d73fad4e6b5d75b82017beff1319
Alan Pieczonka <alanpieczonka@gmail.com>
2020-05-05 21:51:18 +0200

    Rewrite Management page to functional component

--- src/components/pages/Management/index.js
+++ src/components/pages/Management/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 
 import { Switch, Route } from "react-router-dom";
 
@@ -10,23 +10,21 @@ import ManagementSidebar from './Sidebar'
 import Organization from "./Organization"
 import Deals from "./Deals"
 
-class Management extends Component {
-  render() {
-    return (
-      <>
-        <ManagementSidebar />
-        <Content>
-          <Switch>
-            <Route path="/management/edit/:id" component={ProductEdit} />
-            <Route path="/management/new" component={ProductCreate} />
-            <Route path="/management/products" component={ProductList} />
-            <Route path="/management/deals" component={Deals} />
-            <Route path="/management/organization" component={Organization} />
-          </Switch>
-        </Content>
-      </>
-    );
-  }
+const Management = () => {
+  return (
+    <>
+      <ManagementSidebar />
+      <Content>
+        <Switch>
+          <Route path="/management/edit/:id" component={ProductEdit} />
+          <Route path="/management/new" component={ProductCreate} />
+          <Route path="/management/products" component={ProductList} />
+          <Route path="/management/deals" component={Deals} />
+          <Route path="/management/organization" component={Organization} />
+        </Switch>
+      </Content>
+    </>    
+  )
 }
 
 export default Management;
